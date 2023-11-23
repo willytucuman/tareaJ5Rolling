265b32d743d8d869c2f303c194c72d962b671c2c
Nicolas Molina <nicolasmolina0997@hotmail.com>
2023-11-22 21:13:33 -0300

    actualizandoTarea1

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// 1.- Crear un repo en github.
+// 2.- Crear la rama dev.
+// 3.- Vamos a tener solo un archivo index.html y un archivo script.js
+// 4.- Crear una rama por cada integrante del equipo.
+// 5.- Mergeamos las branch a dev.
+
+// Trabajo en equipo
+// Carrito de productos
+// Carrito de compras con Array
+// ■ Crear una aplicación de carrito de compras donde se ingresen
+// productos como elementos en un array.
+// ■ las tareas que debe hacer son las siguientes:
+// ● Agregar producto al carrito
+// ● Listar los productos del carrito
+// ● Buscar un producto en el carrito
+// ● Filtrar productos que coincidan con una palabra, parte de ella
+// o una letra
+// ● Eliminar producto del carrito
+
 const productos = ['television', 'celular', 'tablet', 'reloj'];
 let carrito = [];
 
@@ -7,6 +26,7 @@ const ingresarProductos = (productos) => {
   console.log(carrito);
   listarProductos(carrito);
   buscarProducto(carrito);
+  eliminarProducto(carrito);
 };
 
 const listarProductos = (carrito) => {
@@ -16,9 +36,29 @@ const listarProductos = (carrito) => {
 };
 
 const buscarProducto = (carrito) => {
-  const nuevoProducto = carrito.filter((producto) => {});
+  const nuevoProducto = carrito.filter((producto) => {
+    producto.toLowerCase();
+    return producto === 'celular';
+  });
 
   console.log(nuevoProducto);
 };
 
+const filtrarProductos = (palabraClave) => {
+  productoFiltrado = productos.filter((producto) =>
+    producto.includes(palabraClave)
+  );
+
+  return console.log(productoFiltrado);
+};
+
+const eliminarProducto = (carrito) => {
+  const carritoFiltrado = carrito.filter(
+    (producto) => producto !== 'television'
+  );
+
+  return console.log(carritoFiltrado);
+};
+
 ingresarProductos(productos);
+filtrarProductos('t');
